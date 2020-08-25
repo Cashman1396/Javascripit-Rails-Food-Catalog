acc37f4a4486704757d34ee4a4711434806b01a6
chris_ashman <chrisashman96@gmail.com>
2020-08-25 10:12:53 -0400

    added html and seed

--- food-catalog-backend/db/seeds.rb
+++ food-catalog-backend/db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Catalog.destroy_all
+Food.destroy_all
+
+entrees, desserts, drinks = Catalog.create([
+  {name: "Entrees"}, {name: "Desserts"}, {name: "Drinks"}
+])
+
+entrees.foods.create([
+  {
+    name: "Lemon Poppy Seed Muffin", 
+    cost: 2.25, 
+    description: "Lemon poppy seed muffin.",
+    image_url: "assets/images/lemon-poppyseed-muffin.jpeg"
+  },
+
+  {
+    name: "Dugtrio Sandwich Trio"
+    cost: 1.60,
+    description: "Dugtrio Sandwich Trio",
+    image_url: "assets/images/dugtrio-sandwich.jpg"
+  }
+
+  desserts.foods.create([
+  {
+    name: "Hot Coffee", 
+    cost: 1.50,
+    description: "Hot coffee brewed via French press method served in a 12oz mug.",
+    image_url: "assets/images/hot-coffee.jpg"
+  },
+  {
+    name: "Fluffy Eevee Pancakes",
+    cost: 1.00,
+    description: "Bouncy pancakes that look like eevee"
+    image_url: "assets/images/eevee-pancakes.jpg"
+  }
+   
+  ])
+
+  
+
+])
